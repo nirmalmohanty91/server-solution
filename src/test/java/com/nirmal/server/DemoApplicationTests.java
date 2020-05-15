c4c919c3c7e73228f265e2a12bb6163922a53f32
Nirmal <nirmalmhnt@gmail.com>
2020-05-16 00:43:51 +0530

    initial commit

---
+++ src/test/java/com/nirmal/server/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmal.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
